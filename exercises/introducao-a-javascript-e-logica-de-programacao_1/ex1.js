8737c4b0004c4cb4c60accfc05d3c41fd704ffb4
jonathan <jonathanfernandesjf98@gmail.com>
2021-02-24 20:23:29 -0300

    exercicio 1 operadores aritméticos básicos

---
+++ exercises/introducao-a-javascript-e-logica-de-programacao_1/ex1.js
@@ -0,0 +1,17 @@
+let a = 10;
+let b = 15;
+
+let soma = a + b;
+console.log('a soma das variáveis é igual a',soma);
+
+let subtracao = a - b;
+console.log('a subtracao das variáveis é igual a',subtracao);
+
+let multi = a * b;
+console.log('a multiplicação das variáveis é igual a',multi);
+
+let div = a / b;
+console.log('a divisão das variáveis é igual a',div);
+
+let mod = a % b;
+console.log('o módulo das variaveis é igual a',mod);
