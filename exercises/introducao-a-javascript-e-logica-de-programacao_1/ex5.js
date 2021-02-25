4ac10ad4eeb6378c4d6fc0465917d6aecc11de27
jonathan <jonathanfernandesjf98@gmail.com>
2021-02-25 10:46:35 -0300

    soma dos 3 valores de um triangulo, se igual a 180 true, se diferente false.

---
+++ exercises/introducao-a-javascript-e-logica-de-programacao_1/ex5.js
@@ -0,0 +1,13 @@
+let ang1 = 60;
+let ang2 = 60;
+let ang3 = 60;
+
+if (ang1 + ang2 + ang3 == 180) {
+console.log('True')
+}
+else if (ang1 + ang2 + ang3 != 180) {
+console.log('False')
+}
+else {
+console.log('Erro')    
+}
