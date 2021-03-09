fa982996ab8e5d39939b56c53e804831827a9d6e
jonathan <jonathanfernandesjf98@gmail.com>
2021-03-09 11:53:58 -0300

    função para criar lista com os dias do mes

--- exercises/javascript-dom-eventos-e-web-storage_3/scripts1.js
+++ exercises/javascript-dom-eventos-e-web-storage_3/scripts1.js
@@ -14,7 +14,16 @@ function createDaysOfTheWeek() {
   createDaysOfTheWeek();
   
   // Escreva seu código abaixo.
-
+function createDaysOfTheMonth (){
   const dezDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
+  const daysOfMonth = document.querySelector('#days');
 
-  
+for (index = 0; index < dezDaysList.length; index += 1){
+  const daysMonth = dezDaysList[index];
+  const monthDaysList = document.createElement('li');
+
+  monthDaysList.innerText = daysMonth;
+  daysOfMonth.appendChild(monthDaysList);
+}
+}
+createDaysOfTheMonth();
