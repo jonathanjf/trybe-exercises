d16cdd808aee4a9751ee980767ad4282e62fbc9b
jonathan <jonathanfernandesjf98@gmail.com>
2021-03-24 13:33:06 -0300

    exercício dois parte um

--- exercises/introducao-a-javascript-es6-e-testes-unitarios_1/ex1.js
+++ exercises/introducao-a-javascript-es6-e-testes-unitarios_1/ex1.js
@@ -1,3 +1,4 @@
+// exercício um parte um
 const testingScope = (escopo) => {
     if (escopo === true) {
         let ifScope = `Não devo ser utilizada fora do meu escopo (if)`;
@@ -7,7 +8,24 @@ const testingScope = (escopo) => {
         let elseScope = `Não devo ser utilizada fora meu escopo (else)`;
         console.log(elseScope);
     }
-    // console.log(ifScope + ' o que estou fazendo aqui ? :O'); // Se necessário esta linha pode ser removida.
+
 }
 
 testingScope(true);
+
+// Exercício dois parte um
+
+const oddsAndEvens = [13, 3, 4, 10, 7, 2];
+// Seu código aqui.
+
+//função exercício dois parte um no modelo arrow function
+oddsAndEvens.sort((a, b) => a - b)
+console.log(oddsAndEvens);
+
+//função exercício dois parte um no modelo função anônima
+oddsAndEvens.sort(function (a, b) {
+    return a - b
+});
+console.log(`${oddsAndEvens} se encontram ordenados de forma crescente!`)
+
+//Help, comente uma das funções do exercício dois para ficar mais claro o funcionamento de cada uma.
