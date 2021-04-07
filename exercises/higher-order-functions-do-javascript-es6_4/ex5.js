2e912362b755f4321c24bbb43f18b0bbb43d7874
jonathan <jonathanfernandesjf98@gmail.com>
2021-04-06 21:12:05 -0300

    exercícios dia 4 bloco 8 até o exercício 4 completo

---
+++ exercises/higher-order-functions-do-javascript-es6_4/ex5.js
@@ -0,0 +1,17 @@
+
+const assert = require('assert');
+
+const names = [
+    'Aanemarie', 'Adervandes', 'Akifusa',
+    'Abegildo', 'Adicellia', 'Aladonata',
+    'Abeladerco', 'Adieidy', 'Alarucha',
+];
+
+function containsA() {
+    // escreva seu código aqui
+    return names.reduce((prValue, nxtValue, index, array) => {
+
+    })
+}
+
+assert.deepStrictEqual(containsA(), 20);
